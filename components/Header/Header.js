c8f90c7eb1b8a26bc3b1387023a1d1d3cffbad48
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-23 11:41:44 +0100

    add menu to display on clicking hamburger icon

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,11 +2,13 @@ class Header {
   constructor(element) {
     this.element = element;
     this.hambuger = this.element.querySelector('#hamburger-icon');
+    this.mobileNav = this.element.querySelector('.mobile-nav');
     this.hambuger.addEventListener('click', this.toggleActive);
   }
 
   toggleActive = () => {
     this.hambuger.classList.toggle('active');
+    this.mobileNav.classList.toggle('active');
   }
 }
 
