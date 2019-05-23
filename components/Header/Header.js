73c7f23ce61ae27163bfc9d3460ae7f12384400d
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-23 16:01:11 +0100

    add scroll to top button

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,9 +1,19 @@
 class Header {
   constructor(element) {
     this.element = element;
+    this.backToTop = document.querySelector('#back-to-top');
     this.hambuger = this.element.querySelector('#hamburger-icon');
     this.mobileNav = this.element.querySelector('.mobile-nav');
     this.hambuger.addEventListener('click', this.toggleActive);
+    window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      this.backToTop.style.visibility = 'visible';
+      this.backToTop.style.opacity = '1';
+    } else {
+      this.backToTop.style.visibility = 'hidden';
+      this.backToTop.style.opacity = '0';
+    }
+  });
   }
 
   toggleActive = () => {
